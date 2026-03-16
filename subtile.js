230d6066068f728b03869b5b728cf884ab31cb3c
hipstershiba <vitorrizzato@gmail.com>
2026-03-16 20:10:19 -0300

    Tone down bright tile whites and improve canvas/background separation

--- subtile.js
+++ subtile.js
@@ -1,3 +1,28 @@
+const SUBTILE_MAX_SAFE_CHANNEL = 230;
+const SUBTILE_DEFAULT_OFFWHITE = [226, 224, 220];
+
+function clampSubtileColorToSafeRange(inputColor) {
+    let c = inputColor;
+    if (c === undefined || c === null) {
+        c = color(SUBTILE_DEFAULT_OFFWHITE[0], SUBTILE_DEFAULT_OFFWHITE[1], SUBTILE_DEFAULT_OFFWHITE[2]);
+    }
+
+    let r = red(c);
+    let g = green(c);
+    let b = blue(c);
+    let a = alpha(c);
+
+    let peak = max(r, g, b);
+    if (peak > SUBTILE_MAX_SAFE_CHANNEL) {
+        let scale = SUBTILE_MAX_SAFE_CHANNEL / peak;
+        r *= scale;
+        g *= scale;
+        b *= scale;
+    }
+
+    return color(round(r), round(g), round(b), a);
+}
+
 class Subtile {
     constructor(w, h, type) {
         this.w = w;
@@ -5,18 +30,18 @@ class Subtile {
         // Padding based on smaller dimension or average? Let's use smaller dimension.
         this.padding = min(w, h) * 0.05;
         this.type = type;
-        this.color = color(256, 256, 256);
+        this.color = clampSubtileColorToSafeRange(color(255, 255, 255));
     }
 
     set_color(color) {
-        this.color = color;
+        this.color = clampSubtileColorToSafeRange(color);
     }
 
     render(ctx, x, y, customColor) {
         ctx.push();
         ctx.translate(x, y);
         
-        let c = customColor !== undefined ? customColor : this.color;
+        let c = customColor !== undefined ? customColor : clampSubtileColorToSafeRange(this.color);
 
         if (typeof TILE_RENDERERS !== 'undefined' && TILE_RENDERERS[this.type]) {
              TILE_RENDERERS[this.type](ctx, this.w, this.h, this.padding, c);
